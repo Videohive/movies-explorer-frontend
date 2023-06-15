3955dcbd460c84003b7e46666769d88a4b9f16d2
Timur Harchenko <support.solomon@yandex.ru>
2023-06-15 20:10:07 +0400

    add 404

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
 
 import Header from "../Header/Header.jsx";
 import Footer from "../Footer/Footer.jsx";
@@ -9,6 +9,17 @@ import SavedMovies from "../SavedMovies/SavedMovies.jsx";
 import Profile from "../Profile/Profile.jsx";
 import Register from "../Register/Register.jsx";
 import Login from "../Login/Login.jsx";
+import NotFound from "../NotFound/NotFound.jsx";
+
+function NotFoundWithBackButton() {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  return <NotFound goBack={goBack} />;
+}
 
 export default function App() {
 
@@ -23,6 +34,7 @@ export default function App() {
           <Route path="/profile" element={<Profile />} />
           <Route path="/signup" element={<Register />} />
           <Route path="/signin" element={<Login />} />
+          <Route path="*" element={<NotFoundWithBackButton />} />
         </Routes>
         <Footer />
       </div>
